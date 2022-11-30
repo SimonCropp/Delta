3b26350134c3327a45d4e8c7774c56fd1df855bf
Simon <simon.cropp@gmail.com>
2022-11-30 12:56:03 +1100

    .

--- src/Efficiency/ChangeTrackingSequenceNumber.cs
+++ src/Efficiency/ChangeTrackingSequenceNumber.cs
@@ -4,6 +4,56 @@ namespace Efficiency;
 
 public static class ChangeTrackingSequenceNumber
 {
+    public static async Task EnableTracking(this DbConnection connection)
+    {
+        if (await IsTrackingEnabled(connection))
+        {
+            return;
+        }
+
+        await using var trackChangesOnDbCommand = connection.CreateCommand();
+        trackChangesOnDbCommand.CommandText = $@"
+alter database {connection.Database}
+set change_tracking = on
+(
+    change_retention = 30 days,
+    auto_cleanup = on
+)";
+        await trackChangesOnDbCommand.ExecuteNonQueryAsync();
+    }
+
+    public static async Task<bool> IsTrackingEnabled(this DbConnection connection)
+    {
+        var dbNames = await GetDatabasesWithTracking(connection);
+        // Enable tracked changes if this DB is not in the list
+        return dbNames.Contains(connection.Database);
+    }
+
+    public static async Task DisableTracking(this DbConnection connection)
+    {
+        await using var command = connection.CreateCommand();
+        command.CommandText = $"ALTER DATABASE [{connection.Database}] SET CHANGE_TRACKING = OFF;";
+        await command.ExecuteNonQueryAsync();
+    }
+
+    public static async Task<IReadOnlyList<string>> GetDatabasesWithTracking(this DbConnection connection)
+    {
+        await using var command = connection.CreateCommand();
+        command.CommandText = @"
+select d.name
+from sys.databases as d inner join
+     sys.change_tracking_databases as t on
+     t.database_id = d.database_id";
+        await using var reader = await command.ExecuteReaderAsync();
+        var list = new List<string>();
+        while (await reader.ReadAsync().ConfigureAwait(false))
+        {
+            list.Add((string) reader[0]);
+        }
+
+        return list;
+    }
+
     public static void AddRowVersionProperty<T>(this EntityTypeBuilder<T> builder)
         where T : class, IRowVersion =>
         builder.Property(nameof(IRowVersion.RowVersion))
@@ -40,35 +90,6 @@ else
         try
         {
             return (string) (await command.ExecuteScalarAsync(token))!;
-        }
-        finally
-        {
-            await connection.CloseAsync();
-        }
-    }
-
-    static async Task<T> ExecuteScalarOnCurrentConnection<T>(this DbContext context, string commandText, CancellationToken token = default)
-    {
-        // Do not dispose of this connection as it kill the context
-        var connection = context.Database.GetDbConnection();
-        await using var command = connection.CreateCommand();
-        var transaction = context.Database.CurrentTransaction;
-        if (transaction != null)
-        {
-            command.Transaction = transaction.GetDbTransaction();
-        }
-
-        command.CommandText = commandText;
-
-        if (connection.State != ConnectionState.Closed)
-        {
-            return (T) (await command.ExecuteScalarAsync(token))!;
-        }
-
-        await connection.OpenAsync(token);
-        try
-        {
-            return (T) (await command.ExecuteScalarAsync(token))!;
         }
         finally
         {
--- src/Tests/Usage.cs
+++ src/Tests/Usage.cs
@@ -1,7 +1,8 @@
-﻿public class Usage : LocalDbTestBase
+﻿public class Usage :
+    LocalDbTestBase
 {
     [Test]
-    public async Task LastTimeStamp()
+    public async Task LastTimeStampRowVersion()
     {
         await using var database = await LocalDb();
 
@@ -16,5 +17,49 @@
         var newTimeStamp = await database.Context.LastTimeStamp();
         Assert.IsNotEmpty(newTimeStamp);
         Assert.IsNotNull(newTimeStamp);
+    }
+
+    [Test]
+    public async Task LastTimeStampRowVersionAndTracking()
+    {
+        await using var database = await LocalDb();
+
+        await database.Connection.EnableTracking();
+        var timeStamp = await database.Context.LastTimeStamp();
+        Assert.IsNotEmpty(timeStamp);
+        Assert.IsNotNull(timeStamp);
+        var entity = new Company
+        {
+            Content = "The company"
+        };
+        await database.AddDataUntracked(entity);
+        var newTimeStamp = await database.Context.LastTimeStamp();
+        Assert.IsNotEmpty(newTimeStamp);
+        Assert.IsNotNull(newTimeStamp);
+    }
+
+    [Test]
+    public async Task GetDatabasesWithTracking()
+    {
+        await using var database = await LocalDb();
+        await database.Connection.EnableTracking();
+        Assert.IsNotEmpty(await database.Connection.GetDatabasesWithTracking());
+    }
+
+    [Test]
+    public async Task DisableTracking()
+    {
+        await using var database = await LocalDb();
+        await database.Connection.EnableTracking();
+        await database.Connection.DisableTracking();
+        Assert.IsFalse(await database.Connection.IsTrackingEnabled());
+    }
+
+    [Test]
+    public async Task IsTrackingEnabled()
+    {
+        await using var database = await LocalDb();
+        await database.Connection.EnableTracking();
+        Assert.IsTrue(await database.Connection.IsTrackingEnabled());
     }
 }
