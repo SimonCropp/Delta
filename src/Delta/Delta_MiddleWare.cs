74b3c1bb5cf33e227e883c8d79cd9d7c49f33122
Simon <simon.cropp@gmail.com>
2022-12-01 11:13:06 +1100

    .

--- src/Delta/Delta_MiddleWare.cs
+++ src/Delta/Delta_MiddleWare.cs
@@ -4,7 +4,7 @@ public static partial class Delta
 {
     static string assemblyWriteTime = File.GetLastWriteTime(Assembly.GetEntryAssembly()!.Location).Ticks.ToString();
 
-    public static IApplicationBuilder UseDelta<T>(this IApplicationBuilder builder, Func<HttpContext, string>? suffix = null)
+    public static IApplicationBuilder UseDelta<T>(this IApplicationBuilder builder, Func<HttpContext, string?>? suffix = null)
         where T : DbContext =>
         builder.Use(async (context, next) =>
         {
@@ -16,39 +16,39 @@ public static partial class Delta
             await next();
         });
 
-    public static ComponentEndpointConventionBuilder UseDelta<TDbContext>(this ComponentEndpointConventionBuilder builder, Func<HttpContext, string>? suffix = null)
+    public static ComponentEndpointConventionBuilder UseDelta<TDbContext>(this ComponentEndpointConventionBuilder builder, Func<HttpContext, string?>? suffix = null)
         where TDbContext : DbContext =>
         builder.UseDelta<ComponentEndpointConventionBuilder, TDbContext>(suffix);
 
-    public static ConnectionEndpointRouteBuilder UseDelta<TDbContext>(ConnectionEndpointRouteBuilder builder, Func<HttpContext, string>? suffix = null)
+    public static ConnectionEndpointRouteBuilder UseDelta<TDbContext>(ConnectionEndpointRouteBuilder builder, Func<HttpContext, string?>? suffix = null)
         where TDbContext : DbContext =>
         builder.UseDelta<ConnectionEndpointRouteBuilder, TDbContext>(suffix);
 
-    public static ControllerActionEndpointConventionBuilder UseDelta<TDbContext>(this ControllerActionEndpointConventionBuilder builder, Func<HttpContext, string>? suffix = null)
+    public static ControllerActionEndpointConventionBuilder UseDelta<TDbContext>(this ControllerActionEndpointConventionBuilder builder, Func<HttpContext, string?>? suffix = null)
         where TDbContext : DbContext =>
         builder.UseDelta<ControllerActionEndpointConventionBuilder, TDbContext>(suffix);
 
-    public static HubEndpointConventionBuilder UseDelta<TDbContext>(this HubEndpointConventionBuilder builder, Func<HttpContext, string>? suffix = null)
+    public static HubEndpointConventionBuilder UseDelta<TDbContext>(this HubEndpointConventionBuilder builder, Func<HttpContext, string?>? suffix = null)
         where TDbContext : DbContext =>
         builder.UseDelta<HubEndpointConventionBuilder, TDbContext>(suffix);
 
-    public static IHubEndpointConventionBuilder UseDelta<TDbContext>(this IHubEndpointConventionBuilder builder, Func<HttpContext, string>? suffix = null)
+    public static IHubEndpointConventionBuilder UseDelta<TDbContext>(this IHubEndpointConventionBuilder builder, Func<HttpContext, string?>? suffix = null)
         where TDbContext : DbContext =>
         builder.UseDelta<IHubEndpointConventionBuilder, TDbContext>(suffix);
 
-    public static PageActionEndpointConventionBuilder UseDelta<TDbContext>(this PageActionEndpointConventionBuilder builder, Func<HttpContext, string>? suffix = null)
+    public static PageActionEndpointConventionBuilder UseDelta<TDbContext>(this PageActionEndpointConventionBuilder builder, Func<HttpContext, string?>? suffix = null)
         where TDbContext : DbContext =>
         builder.UseDelta<PageActionEndpointConventionBuilder, TDbContext>(suffix);
 
-    public static RouteGroupBuilder UseDelta<TDbContext>(this RouteGroupBuilder builder, Func<HttpContext, string>? suffix = null)
+    public static RouteGroupBuilder UseDelta<TDbContext>(this RouteGroupBuilder builder, Func<HttpContext, string?>? suffix = null)
         where TDbContext : DbContext =>
         builder.UseDelta<RouteGroupBuilder, TDbContext>(suffix);
 
-    public static RouteHandlerBuilder UseDelta<TDbContext>(this RouteHandlerBuilder builder, Func<HttpContext, string>? suffix = null)
+    public static RouteHandlerBuilder UseDelta<TDbContext>(this RouteHandlerBuilder builder, Func<HttpContext, string?>? suffix = null)
         where TDbContext : DbContext =>
         builder.UseDelta<RouteHandlerBuilder, TDbContext>(suffix);
 
-    public static TBuilder UseDelta<TBuilder, TDbContext>(this TBuilder builder, Func<HttpContext, string>? suffix = null)
+    public static TBuilder UseDelta<TBuilder, TDbContext>(this TBuilder builder, Func<HttpContext, string?>? suffix = null)
         where TBuilder : IEndpointConventionBuilder
         where TDbContext : DbContext =>
         builder.AddEndpointFilter(async (context, next) =>
@@ -61,7 +61,7 @@ public static partial class Delta
             return await next(context);
         });
 
-    static async Task<bool> HandleRequest<T>(HttpContext context, Func<HttpContext, string>? suffix)
+    static async Task<bool> HandleRequest<T>(HttpContext context, Func<HttpContext, string?>? suffix)
         where T : DbContext
     {
         var request = context.Request;
@@ -79,9 +79,10 @@ public static partial class Delta
         var data = context.RequestServices.GetRequiredService<T>();
         var rowVersion = await data.GetLastTimeStamp();
         var etag = $"{assemblyWriteTime}-{rowVersion}";
-        if (suffix != null)
+        var suffixValue = suffix?.Invoke(context);
+        if (suffixValue != null)
         {
-            etag += "-" + suffix(context);
+            etag += "-" + suffixValue;
         }
 
         response.Headers.Add("ETag", $"\"{etag}\"");
