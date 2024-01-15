287bc65bb7cb29d13383ee0486883b66bc549edf
Simon Cropp <simon.cropp@gmail.com>
2024-01-15 15:12:54 +1100

    Update DeltaExtensions.cs

--- src/Delta/DeltaExtensions.cs
+++ src/Delta/DeltaExtensions.cs
@@ -220,9 +220,9 @@ declare @changeTracking bigint = change_tracking_current_version();
 declare @timeStamp bigint = convert(bigint, @@dbts);
 
 if (@changeTracking is null)
-  select cast(@timeStamp as varchar) as nocount
+  select cast(@timeStamp as varchar)
 else
-  select cast(@timeStamp as varchar) + '-' + cast(@changeTracking as varchar) as nocount
+  select cast(@timeStamp as varchar) + '-' + cast(@changeTracking as varchar)
 -- end-snippet
 ";
         return (string) (await command.ExecuteScalarAsync(cancel))!;
