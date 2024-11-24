20d6f51882f892de59ede4d9a1769ee86b34ad5b
Simon Cropp <simon.cropp@gmail.com>
2024-11-24 21:09:01 +1100

    Update Program.cs

--- src/WebApplication/Program.cs
+++ src/WebApplication/Program.cs
@@ -1,3 +1,5 @@
+using System.Data;
+
 SqlInstance sqlInstance = new(
     name: "DeltaWebApplication",
     buildTemplate: DbBuilder.Create);
@@ -8,14 +10,51 @@ var connectionString = database.ConnectionString;
 #region UseDelta
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddScoped<SqlConnection>(_ => new(connectionString));
+builder.Services.AddScoped(_ => new SqlConnection(connectionString));
 var app = builder.Build();
 app.UseDelta(
     getConnection: httpContext => httpContext.RequestServices.GetRequiredService<SqlConnection>());
 
 #endregion
 
-app.MapGet("/", () => "Hello World!");
+await using var command = database.Connection.CreateCommand();
+command.CommandText =
+    $"""
+     insert into [Companies] (Id, Content)
+     values ('{Guid.NewGuid()}', 'The company')
+     """;
+await command.ExecuteNonQueryAsync();
+
+app.MapGet("/", async _ =>
+{
+    var connection = _.RequestServices.GetRequiredService<SqlConnection>();
+
+    if (connection.State == ConnectionState.Closed)
+    {
+        await connection.OpenAsync();
+    }
+
+    await using var command = connection.CreateCommand();
+    command.CommandText = "select * from Companies";
+    await using var reader = await command.ExecuteReaderAsync();
+    var builder = new StringBuilder("Results: ");
+    builder.AppendLine();
+    while (await reader.ReadAsync())
+    {
+        for (var i = 0; i < reader.FieldCount; i++)
+        {
+            var value = reader.GetValue(i);
+            if (value is byte[] bytes)
+            {
+                value = BitConverter.ToString(bytes);
+            }
+
+            builder.AppendLine($"{reader.GetName(i)}: {value}");
+        }
+    }
+
+    await _.Response.WriteAsync(builder.ToString());
+});
 
 #region UseDeltaMapGroup
 
